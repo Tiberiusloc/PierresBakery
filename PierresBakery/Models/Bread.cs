fa8af0f57957f1b036d1177f1e76c25207fc4bd8
Tiberius Lockett <tlockett680@gmail.com>
2022-09-23 10:37:27 -0700

    add Bread and Pastry constructors

---
+++ PierresBakery/Models/Bread.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PierresBakery.Models
+{
+  public class Bread
+  {
+    public Bread()
+    {
+      Console.WriteLine("Alive");
+    }
+  }
+}
---
+++ PierresBakery/Models/Pastry.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PierresBakery.Models
+{
+  public class Pastry
+  {
+    public Pastry()
+    {
+      Console.WriteLine("born");
+    }
+  }
+}
